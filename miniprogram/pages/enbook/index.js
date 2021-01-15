599175e7c994be27b8f2ad125f5439642fae3b50
wulala <189267167@qq.com>
2021-01-15 18:05:03 +0800

    feat: 新增英语书籍阅读

---
+++ miniprogram/pages/enbook/index.js
@@ -0,0 +1,30 @@
+// miniprogram/pages/enbook/index.js
+Page({
+
+    data: {
+
+        chapter: {
+            en: ['Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest.',
+                'It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing.',
+                'In the book it said: "Boa constrictors swallow their prey whole, without chewing it.',
+                'After that they are not able to move, and they sleep through the six months that they need for digestion."',
+                'I pondered deeply, then, over the adventures of the jungle.',
+                'And after some work with a coloured pencil, I succeeded in making my first drawing.'],
+            cn: ['当我还只有六岁的时候，在一本描写原始森林，名叫《自然界真实的故事》的书中，看到了一幅精彩的插画。',
+                '画的是一条蟒蛇正在吞食一只大野兽。这就是那副画的摹本。',
+                '这本书中写道：“这些蟒蛇把它们的猎物不加咀嚼地吞下。',
+                '一会儿后就不能再动弹了，它们就在长长的六个月的睡眠中消化这些食物。”',
+                '当时，我对丛林中的奇遇想得很多。',
+                '我用彩色铅笔画出了我的第一幅画作。',
+                '我的第一号作品，它是这样的：'],
+            mp3: 'https://6675-funnywx-1302887224.tcb.qcloud.la/little_prince/chapter1.mp3?sign=5e4cc40c5d2add4b00c863dc6d172a27&t=1610704169',
+            time: '3:11',
+        },
+
+    },
+
+    onLoad(options) { },
+
+    // eslint-disable-next-line no-empty-function
+    onShareAppMessage() { },
+})
